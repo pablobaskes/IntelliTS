69679fcd53799fa4a97fe24bf1ae30dd33a426f2
Pablo Vazquez <pablovazquezbarrena97@gmail.com>
2023-09-12 19:56:12 +0200

    .env added

--- inteli-front/src/utils/settings.ts
+++ inteli-front/src/utils/settings.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv';
 
-dotenv.config();
+export const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
+export const TMDB_TOKEN = import.meta.env.VITE_REACT_APP_TMDB_TOKEN;
 
 export const BASE_URL = "https://api.themoviedb.org/3";
-export const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const IMAGE_BASE_URLw92 = "https://image.tmdb.org/t/p/w92";
 export const IMAGE_BASE_URLw154 = "https://image.tmdb.org/t/p/w154";
 export const IMAGE_BASE_URLw185 = "https://image.tmdb.org/t/p/w185";
@@ -16,4 +16,3 @@ export const USER_URL = `${LOCAL_BASE_URL}/user`;
 export const LIST_URL = `${LOCAL_BASE_URL}/list`;
 export const LIST_ITEM_URL = `${LOCAL_BASE_URL}/listItem`;
 
-export const TMDB_TOKEN = process.env.REACT_APP_TMDB_TOKEN;
